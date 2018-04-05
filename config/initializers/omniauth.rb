68e19c47f94a84b645e00ee769d6fe05dba944aa
reginayiwang <reginawang@live.com>
2018-04-05 10:31:54 -0700

    Update omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :twitter, '6HVP0Msmp1fvmUMJf8cNHLe1i', '9grQRaCRgeXbaKHgkXnf9PqXynbGEUOKT1Z5wq8pdue7IYGixg'
+    provider :twitter, 'KEY', 'SECRET'
 end
